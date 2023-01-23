afc71b78732af57bfbeee5944250cf9f04352c70
forgetwst <123350008+forgetwst@users.noreply.github.com>
2023-01-23 20:02:25 +0300

    Add files via upload

---
+++ дз.vb
@@ -0,0 +1,16 @@
+﻿Imports System
+
+Module Program
+
+    Sub Main(args As String())
+        Dim n As Integer, Pr As Long = 1
+
+        Console.WriteLine("Привет, я перемножаю числа! Хочешь что-нибудь умножить?")
+
+        Do
+            n = Console.ReadLine()
+            Pr = Pr * n
+            Console.WriteLine("Произведение чисел=", Pr)
+        Loop Until n = 1
+    End Sub
+End Module
