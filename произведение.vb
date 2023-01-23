f543e391243662845395bbf8d4068e4583bfecff
forgetwst <123350008+forgetwst@users.noreply.github.com>
2023-01-23 09:42:22 +0300

    Add files via upload

---
+++ произведение.vb
@@ -0,0 +1,21 @@
+﻿Imports System
+Imports System.Linq.Expressions
+
+Module Program
+    Sub Main(args As String())
+        Dim C As Integer
+        Dim Pr As Integer = 1
+        Do
+            Console.WriteLine("Введите множитель: ")
+            C = Console.ReadLine()
+            If C < Int32.MaxValue / Pr Then
+                Pr = C * Pr
+            Else
+                Console.WriteLine("!Переполнение памяти!")
+                Exit Do
+            End If
+        Loop Until C = 1
+        Console.WriteLine("Произведение = {0}", Pr)
+    End Sub
+End Module
+
