4dc4c40b79b259310f74b5fecc93224b0a008a0c
forgetwst <123350008+forgetwst@users.noreply.github.com>
2023-01-26 20:14:28 +0300

    Add files via upload

---
+++ корова и бык.vb
@@ -0,0 +1,21 @@
+﻿Imports System
+
+Module Coas
+    Sub Main(args As String())
+        Dim number, ver As String
+        'TOOO: генерируем число
+        number = "1234"
+        Console.WriteLine("Угадай число")
+        Do
+            ver = Console.ReadLine()
+            For i = 0 To 3
+                If i = 0 Then
+                    Console.WriteLine("Первая цифра вашей версии = бык")
+                Else
+                    Console.WriteLine("Первая цифра вашей версии = корова")
+                End If
+                End If
+            Next
+        Loop Until number = ver
+    End Sub
+End Module
