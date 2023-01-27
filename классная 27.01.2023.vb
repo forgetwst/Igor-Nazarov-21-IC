d614acbeecbd759ed55317b42179db12a88a1c2c
forgetwst <123350008+forgetwst@users.noreply.github.com>
2023-01-27 09:47:18 +0300

    Add files via upload

---
+++ классная 27.01.2023.vb
@@ -0,0 +1,12 @@
+﻿Module Program
+    Sub Main(args As String())
+        Dim left As Integer = 20
+        Console.SetCursorPosition(l, 10)
+        For j = 0 To 5
+            For i As Integer = 0 To 10
+                Console.Write("H")
+            Next
+            Console.SetCursorPosition(l, Console.GetCursorPosition().Top + 1)
+        Next
+    End Sub
+End Module
