f2da2561e1dd04165031e941402b030f5f431a5c
forgetwst <123350008+forgetwst@users.noreply.github.com>
2023-01-30 09:38:47 +0300

    Add files via upload

---
+++ risyem.vb
@@ -0,0 +1,27 @@
+﻿Module Program
+    Private brush As Char = "0"c
+    Sub Main(args As String(()
+
+        Console.SetWindowPosition(100, 50)
+
+        Hline(10, 5, 70)
+
+        Console.WriteLine()
+
+    End Sub
+
+    Sub Hline(leftX As Byte, topY As Byte, length As Byte)
+        Console.SetCursorPosition(leftX, topY)
+        For i = 1 To length
+            Console.Write(brush)
+        Next
+    End Sub
+    Sub Hline(leftX As Byte, topY As Byte, length As Byte)
+        Console.SetCursorPosition(leftX, topY)
+        For i = 1 To length - 1
+            Console.Write(brush)
+            Console.SetCursorPosition(leftX, topY + i)
+        Next
+    End Sub
+
+End Module
